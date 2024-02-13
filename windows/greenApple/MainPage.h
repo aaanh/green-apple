b1820c1527fbe5c5e4ea848c0004ddad03abc25d
Anh H Nguyen <AnhNguyen@aaanh.com>
2024-02-13 01:12:54 -0500

    init

---
+++ windows/greenApple/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::greenApple::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::greenApple::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
